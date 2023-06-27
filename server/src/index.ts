10122501256361ac7ea6218d51e9dfba26a5b082
tameemalaa <tameem.alaa@gmail.com>
2023-06-27 17:28:24 +0300

    feat: updated port import

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,12 +5,12 @@ import userRoutes from  './routes/user';
 import dotenv from 'dotenv' ;
 
 dotenv.config();
+const PORT: number = parseInt(process.env.PORT || '3000', 10)
 const prisma = getPrismaClient();
 const app = express();
 app.use(express.json());
 app.use(cors({origin: '*',}));
 app.use('/user', userRoutes);
-
-app.listen(process.env.PORT, (): void => {
+app.listen(PORT, (): void => {
   console.log(`Server listening on port ${process.env.PORT}`);
 });
